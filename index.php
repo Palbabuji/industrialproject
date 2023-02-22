db7ff226e73256c6944c3931cce06853b89d9411
Sajalpal <126089317+Palbabuji@users.noreply.github.com>
2023-02-22 22:12:34 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="icon" href="img/logo.gif">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- header -->
+    <?php include("include/header.php") ?>
+   
+    <!-- navbar -->
+    <?php include("include/navbar.php") ?>
+
+    <!-- navbar -->
+    <?php include("include/scroller.php") ?>
+
+
+    <div class="banner">
+        <div class="content">
+        <div class="section">
+        <p>Famous  Engineering <span> Project</span></p>
+        <p><span>Manegement </span> Cousultancy</p>
+        <span>Lorem ipsum dolor sit amet consectetur impedit ipsam sequi inventore ratione excepturi alias repudiandae unde aliquam natus!</span>
+        <div class="banner-btn">
+            <a href="experience.php"><button>What we do</button></a>
+            <a href="contact.php"><button>Contact us</button></a>
+        </div>
+        </div>
+        <div class="video">
+            <video muted loop autoplay id="myVideo">
+                <source src="img/nature.mp4" type="video/mp4">
+            </video>
+        </div>
+        </div>
+    </div>
+
+    <div class="main-body">
+        <div class="about">
+        <span id="about-head">About us</span> <br>
+        <span id="about-section">We are an <label>Engineering Project and Manegement </label> Cousultancy.</span> <br>
+        <br><br>
+        <label>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum a porro omnis! Asperiores, provident id repellendus voluptatibus in sequi distinctio quod iure laborum perspiciatis assumenda, quia minus exercitationem ducimus. A.</label> <br>
+        <label>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum a porro omnis! Asperiores, provident id repellendus voluptatibus in sequi distinctio quod iure laborum perspiciatis assumenda, quia minus exercitationem ducimus. A.</label>  <br>
+        <div class="about-view">
+        <a href="#">View More</a> 
+        </div>
+        </div>
+
+        <div class="about-img">
+            <img src="img/dumbal.jpg" alt="">
+        </div>
+
+        <div class="mission">
+            <div class="bekar">
+            <span id="mission-head">Our mission </span> <br>
+            <span id="mission-section"> We support projects by established a <span>local presence at the doorstep </span> of our clients.</span>
+            <br><br><br>
+            <label>Provident id repellendus voluptatibus in sequi distinctio quod iure laborum perspiciatis assumenda, quia minus exercitationem ducimus.</label> <br>
+            <div class="mission-view">
+            <a href="#">View More</a>             
+        </div>
+        </div>
+        </div>
+
+        <div class="experience">
+            <div class="bekar">
+            <span id="experience-head">Our experience </span> <br>
+            <span id="experience-section"> Our core company is the provition of <span>excellent process engineering </span>service.</span>
+            <br><br><br>
+            <label>Provident id repellendus Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore doloremque cupiditate, voluptates consectetur perferendis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, ex. voluptatibus in sequi distinctio quod iure laborum perspiciatis assumenda, quia minus exercitationem ducimus.</label> <br>
+            <div class="experience-view">
+            <a href="#">View More</a>             
+            </div>
+        </div>
+        </div>
+
+        <div class="brand-img">
+            <img src="img/gymback.jpg" alt="">
+        </div>
+
+        <div class="team">
+            <div class="team-section">
+            <span class="team-head">Our team</span>
+            <h2><b>A core working  group Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat repellat eligendi voluptates similique Design And Project Manegement.</b></h2>
+            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa corrupti voluptas repellendus quisquam asperiores nostrum voluptatibus labore inventore ea commodi.</span>
+            <div class="team-view">
+                <a href="#">Read More</a>             
+            </div>            
+            </div>
+        </div>
+        <div class="blank"></div>
+        <div class="our-services">
+            <h3>CORE SERVICES</h3>
+            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span>
+        </div>
+        <br>
+        
+        
+        <div class="service1">
+            <img src="img/house.png" alt="">
+            <div class="underline"></div>
+            <strong>FEED</strong>
+            <span>STUDY</span>
+                <div class="service-item1">
+                <br>
+                <strong>FEED</strong><br>
+                <span>STUDY</span> <br><br>
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex fuga eligendi molestias illo aut nostrum, quidem sapiente ullam dolor tempore vitae cum ipsam nesciunt animi veniam. Soluta corporis dignissimos nam!</span>
+            </div>
+        </div>
+
+
+        <div class="service2">
+            <img src="img/earth.png" alt="">
+            <div class="underline"></div>
+            <strong>OFFER</strong>
+            <span>OFFER PRIZE</span>
+            <div class="service-item2">
+                <br>
+                <strong>OFFER</strong><br>
+                <span>OFFER PRIZE</span> <br><br>
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex fuga eligendi molestias illo aut nostrum, quidem sapiente ullam dolor tempore vitae cum ipsam nesciunt animi veniam. Soluta corporis dignissimos nam!</span>
+            </div>
+        </div>
+
+
+        <div class="service3">
+            <img src="img/image.png" alt="">
+            <div class="underline"></div>
+            <strong>INDUSTRY</strong>
+            <span>INDUSTRY GROUND</span>
+            <br>
+                <div class="service-item3">
+                <strong>INDUSTRY</strong><br>
+                <span>INDUSTRY GROUND</span><br><br>
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex fuga eligendi molestias illo aut nostrum, quidem sapiente ullam dolor tempore vitae cum ipsam nesciunt animi veniam. Soluta corporis dignissimos nam!</span>
+            </div>
+        </div>
+
+
+        <div class="service4">
+            <img src="img/video.png" alt="">
+            <div class="underline"></div>
+            <strong>VIDEOS</strong>
+            <span>INDUSTRYS VIDEO</span>
+            <br>
+                <div class="service-item4">
+                <br>
+                <strong>VIDEOS</strong><br>
+                <span>INDUSTRYS VIDEO</span><br><br>
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex fuga eligendi molestias illo aut nostrum, quidem sapiente ullam dolor tempore vitae cum ipsam nesciunt animi veniam. Soluta corporis dignissimos nam!</span>
+            </div>
+        </div>
+
+        <h3 class="client-head">OUR CLIENTS</h3>
+        <div class="clients">
+            <?php include("include/logoslider.php") ?>
+        </div>
+
+        <!-- footer -->
+        <?php include("include/footer.php") ?>
+    </div>
+
+    
+    <script src="main.js"></script>
+</body>
+</html>
